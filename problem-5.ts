b81fff0d69b8994e26453ffb4d9d44340e21e45e
gwonyejun <kyjuni1059@gmail.com>
2023-08-08 19:12:51 +0900

    문제 5번

---
+++ problem-5.ts
@@ -0,0 +1,19 @@
+function plusMinus(arr: number[]): void {
+  // Write your code here
+  let max = 0;
+  let min = 0;
+  let zero = 0;
+
+  arr.forEach((x) => {
+    if (x > 0) {
+      max += 1;
+    } else if (x < 0) {
+      min += 1;
+    } else {
+      zero += 1;
+    }
+  })
+  console.log(max / arr.length);
+  console.log(min / arr.length);
+  console.log(zero / arr.length);
+}
