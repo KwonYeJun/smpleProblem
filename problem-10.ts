9b941254b6c528d88ee4bc3ade74348be1114738
gwonyejun <kyjuni1059@gmail.com>
2023-08-10 18:00:15 +0900

    암호화 로직 이해 하기..

--- problem-10.ts
+++ problem-10.ts
@@ -1,25 +1,22 @@
 // 암호화 문제
 
 function encryption(s: string): string {
-  // 공백 제거
+
   const sanitizedString = s.replace(/\s/g, "");
 
-  // 그리드의 행과 열 계산
   const rows = Math.floor(Math.sqrt(sanitizedString.length));
   const cols = Math.ceil(Math.sqrt(sanitizedString.length));
 
-  // 그리드 만들기
   const grid: string[] = [];
   for (let i = 0; i < sanitizedString.length; i += cols) {
       grid.push(sanitizedString.slice(i, i + cols));
   }
 
-  // 그리드의 열을 따라 읽어서 암호화된 메시지 생성
   const encryptedMessage: string[] = [];
   for (let i = 0; i < cols; i++) {
       const encryptedColumn: string[] = [];
       for (const row of grid) {
-          encryptedColumn.push(row[i] || ""); // 열이 부족한 경우 빈 문자열 추가
+          encryptedColumn.push(row[i] || ""); 
       }
       encryptedMessage.push(encryptedColumn.join(""));
   }
@@ -27,7 +24,6 @@ function encryption(s: string): string {
   return encryptedMessage.join(" ");
 }
 
-// 입력 문자열
-const inputString: string = "haveaniceday";
+const inputString: string = "this is ju_ni.5";
 const encryptedResult: string = encryption(inputString);
 console.log(encryptedResult);
