c4010baabe503fa7d6f5c3c5dc66013fd540a7e8
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 15:43:04 +0900

    두 개의 정수 배열이 주어지며, 다음 두 조건을 모두 만족하는 정수를 찾아야 하는 문제이다

---
+++ problem-12.ts
@@ -0,0 +1,27 @@
+
+
+function gcd(a: number, b: number): number {
+  if (b === 0) {
+      return a;
+  }
+  return gcd(b, a % b);
+}
+function lcm(a: number, b: number): number {
+  return (a * b) / gcd(a, b);
+}
+function getTotalX(a: number[], b: number[]): number {
+   const lcm_a = a.reduce(lcm);
+  const gcd_b = b.reduce(gcd);
+
+  let count = 0;
+  for (let i = lcm_a, j = 2; i <= gcd_b; i = lcm_a * j, j++) {
+      if (gcd_b % i === 0) {
+          count++;
+      }
+  }
+  
+  return count;
+}
+
+//첫 번째 배열의 요소가 고려 중인 정수의 약수입니다.
+//고려 중인 정수가 두 번째 배열의 모든 요소의 약수입니다.
