52c66c710c3599478ef5de34109b1d79f7f27a2c
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 15:33:40 +0900

    값을 비교 하여 반환 해준다.

--- problem-11.ts
+++ problem-11.ts
@@ -1,1 +1,13 @@
 // 캥거루가 점프점프
+
+function kangaroo(x1: number, v1: number, x2: number, v2: number): string {
+    if ((v1 > v2) && ((x2 - x1) % (v1 - v2) === 0)) {
+      return "YES";
+  } else {
+      return "NO";
+  }
+}
+
+// Example usage
+console.log(kangaroo(0, 3, 4, 2)); // Output: "YES"
+console.log(kangaroo(0, 2, 5, 3)); // Output: "NO"
