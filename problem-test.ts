75e1627c55935c72d8da468c85fd6303627dc251
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 16:52:26 +0900

    결국 태초 마을

--- problem-test.ts
+++ problem-test.ts
@@ -1,26 +1,16 @@
-function findTranslationDifference(num: number): number {
-  const numStr = num.toString();
-  let maxDifference = 0;
-
-  for (let i = 0; i < numStr.length; i++) {
-      for (let replacement = 0; replacement <= 9; replacement++) {
-          const replacedNum = parseInt(numStr.slice(0, i) + replacement.toString() + numStr.slice(i + 1));
-          const translatedResult = translate(replacedNum);
-
-          const difference = Math.abs(translatedResult - num);
-          maxDifference = Math.max(maxDifference, difference);
-      }
+function findMinMaxDifference(number: number): number {
+  const numberStr = number.toString();
+  let maxNumber = '';
+  let minNumber = '';
+  for (let i = 0; i < numberStr.length; i++) {
+    const digit = parseInt(numberStr[i]);
+    const maxDigit = digit === 1 ? 9 : digit;
+    const minDigit = digit === 8 ? 0 : digit;
+    maxNumber += maxDigit.toString();
+    minNumber += minDigit.toString();
   }
-
-  return maxDifference;
+  return parseInt(maxNumber) - parseInt(minNumber);
 }
-
-function translate(num: number): number {
-  // Simulate the translation function here
-  // For example, replace the 1 digits with 9 as in the example
-  return parseInt(num.toString().replace(/1/g, '9'));
-}
-
-// Example usage
-const num = 909;
-console.log(findTranslationDifference(num)); // Output: 90008
+const inputNumber = 909;
+const difference = findMinMaxDifference(inputNumber);
+console.log(difference);
