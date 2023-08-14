d9a1689117c414b3610d98e0862873399fc2be29
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 16:51:44 +0900

    실패 코드

--- problem-test.ts
+++ problem-test.ts
@@ -1,23 +1,26 @@
 function findTranslationDifference(num: number): number {
   const numStr = num.toString();
-  let maxPossible = numStr;
-  let minPossible = numStr;
+  let maxDifference = 0;
 
   for (let i = 0; i < numStr.length; i++) {
       for (let replacement = 0; replacement <= 9; replacement++) {
-          if (numStr[i] !== replacement.toString()) {
-              const replacedNum = numStr.slice(0, i) + replacement.toString() + numStr.slice(i + 1);
-              if (replacement !== 0 || replacedNum.length === numStr.length) {
-                  maxPossible = Math.max(parseInt(maxPossible), parseInt(replacedNum)).toString();
-                  minPossible = Math.min(parseInt(minPossible), parseInt(replacedNum)).toString();
-              }
-          }
+          const replacedNum = parseInt(numStr.slice(0, i) + replacement.toString() + numStr.slice(i + 1));
+          const translatedResult = translate(replacedNum);
+
+          const difference = Math.abs(translatedResult - num);
+          maxDifference = Math.max(maxDifference, difference);
       }
   }
 
-  return parseInt(maxPossible) - parseInt(minPossible);
+  return maxDifference;
+}
+
+function translate(num: number): number {
+  // Simulate the translation function here
+  // For example, replace the 1 digits with 9 as in the example
+  return parseInt(num.toString().replace(/1/g, '9'));
 }
 
 // Example usage
 const num = 909;
-console.log(findTranslationDifference(num)); // Output: 898
+console.log(findTranslationDifference(num)); // Output: 90008
