1c4059f252d42343438205086cee2738606c7086
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 16:44:18 +0900

    시험 풀기

---
+++ problem-test.ts
@@ -0,0 +1,23 @@
+function findTranslationDifference(num: number): number {
+  const numStr = num.toString();
+  let maxPossible = numStr;
+  let minPossible = numStr;
+
+  for (let i = 0; i < numStr.length; i++) {
+      for (let replacement = 0; replacement <= 9; replacement++) {
+          if (numStr[i] !== replacement.toString()) {
+              const replacedNum = numStr.slice(0, i) + replacement.toString() + numStr.slice(i + 1);
+              if (replacement !== 0 || replacedNum.length === numStr.length) {
+                  maxPossible = Math.max(parseInt(maxPossible), parseInt(replacedNum)).toString();
+                  minPossible = Math.min(parseInt(minPossible), parseInt(replacedNum)).toString();
+              }
+          }
+      }
+  }
+
+  return parseInt(maxPossible) - parseInt(minPossible);
+}
+
+// Example usage
+const num = 111;
+console.log(findTranslationDifference(num)); // Output: 80
