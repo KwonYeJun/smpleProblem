ffdeb29a5542a775040ef8065dd5928476b3143a
gwonyejun <kyjuni1059@gmail.com>
2023-08-14 16:59:43 +0900

    문제 4번

--- problem-test.ts
+++ problem-test.ts
@@ -1,16 +1,1 @@
-function findMinMaxDifference(number: number): number {
-  const numberStr = number.toString();
-  let maxNumber = '';
-  let minNumber = '';
-  for (let i = 0; i < numberStr.length; i++) {
-    const digit = parseInt(numberStr[i]);
-    const maxDigit = digit === 1 ? 9 : digit;
-    const minDigit = digit === 8 ? 0 : digit;
-    maxNumber += maxDigit.toString();
-    minNumber += minDigit.toString();
-  }
-  return parseInt(maxNumber) - parseInt(minNumber);
-}
-const inputNumber = 909;
-const difference = findMinMaxDifference(inputNumber);
-console.log(difference);
+//문제 4번
